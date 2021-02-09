1def71960f100e5e778573d9014994e1e8218a68
ginery <songalingginery@gmail.com>
2021-02-09 21:15:25 +0800

    a

--- digitalBorrowingSystem/App.js
+++ digitalBorrowingSystem/App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import * as React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,100 +7,41 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import {Provider as PaperProvider, TextInput, Button} from 'react-native-paper';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+export default function App() {
+  const [text, setText] = React.useState('');
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <PaperProvider style={styles.wrapper}>
+        <View>
+          <View style={styles.textInputWrapper}>
+            <TextInput
+              label="ID Number"
+              value={text}
+              onChangeText={(text) => setText(text)}
+            />
           </View>
-        </ScrollView>
-      </SafeAreaView>
+        </View>
+      </PaperProvider>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  wrapper: {
+    backgroundColor: 'grey',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // resizeMode: 'cover',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textColor: {
+    color: 'black',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textInputWrapper: {
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
-
-export default App;
