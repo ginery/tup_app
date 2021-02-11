976d496ca4f432c658ef41b6225dddd54f96a8dc
Ginery <songalingginery@gmail.com>
2021-02-11 15:23:32 +0800

    a

--- digitalBorrowingSystem/assets/screen/homeScreen.js
+++ digitalBorrowingSystem/assets/screen/homeScreen.js
@@ -17,7 +17,7 @@ import {
 } from 'react-native-paper';
 //for height of wrapper text info
 const width_proportion = '90%';
-const height_proportion = '40%';
+const btn_wrapper = '45%';
 const img_width = '100%';
 export default function loginScreen() {
   const [text, setText] = React.useState('');
@@ -45,11 +45,74 @@ export default function loginScreen() {
               style={{
                 width: img_width,
                 height: img_width,
-                alignSelf: 'center',
-                resizeMode: 'contain',
+                resizeMode: 'stretch',
+                borderRadius: 2,
               }}
             />
+            <View style={styles.btn_view_wrapper}>
+              <TouchableOpacity
+                style={{
+                  borderWidth: 1,
+                  borderColor: 'black',
+                  width: btn_wrapper,
+                  alignSelf: 'flex-start',
+                  marginTop: 40,
+                  alignItems: 'center',
+                  padding: 20,
+                  borderRadius: 5,
+                  backgroundColor: '#8b8b8b',
+                }}>
+                <Image
+                  source={require('../images/multimeter.png')}
+                  style={{width: 100, height: 100, alignSelf: 'center'}}
+                />
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                  }}>
+                  INSTRUMENT
+                </Text>
+                <View
+                  style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    width: 100,
+                  }}></View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  borderWidth: 1,
+                  borderColor: 'black',
+                  width: btn_wrapper,
+                  alignSelf: 'flex-start',
+                  marginTop: 40,
+                  alignItems: 'center',
+                  padding: 20,
+                  borderRadius: 5,
+                  backgroundColor: '#8b8b8b',
+                }}>
+                <Image
+                  source={require('../images/multimeter.png')}
+                  style={{width: 100, height: 100, alignSelf: 'center'}}
+                />
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                  }}>
+                  INSTRUMENT
+                </Text>
+                <View
+                  style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    width: 100,
+                  }}></View>
+              </TouchableOpacity>
+            </View>
           </View>
+          {/* <View style={styles.textInputWrapper2}>
+            
+          </View> */}
         </View>
       </PaperProvider>
     </>
@@ -67,17 +130,23 @@ const styles = StyleSheet.create({
     color: 'black',
   },
   textInputWrapper: {
-    flex: 1,
-    flexDirection: 'flex-start',
-    justifyContent: 'flex-start',
+    height: 150,
     width: width_proportion,
+    marginBottom: 'auto',
+    marginTop: 80,
+
+    // borderBottomWidth: 5,
+    // borderColor: 'black',
   },
-  wrapperFooter: {
-    alignContent: 'flex-end',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginBottom: 10,
+  textInputWrapper2: {
+    height: 150,
+    width: width_proportion,
+    marginBottom: 'auto',
+    top: 0,
+    borderBottomWidth: 5,
+    borderColor: 'black',
   },
+
   bottom: {
     position: 'absolute',
     left: 0,
@@ -85,5 +154,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     top: 1,
     backgroundColor: '#820100',
+  },
+  btn_view_wrapper: {
+    backgroundColor: 'steelblue',
   },
 });
